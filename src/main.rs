9578453ef72bffa376b1e8f037f16cf7a7e24d7b
gudakesh07 <riturajgudakesh@gmail.com>
2025-08-02 01:26:21 +0530

    gg

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,60 @@
-// mod hola;
-// fn main(){
-//     let x = 56;
-//     let y: u32 = 340000;
-//     let z = 1000.001;
-//     println!("x: {}, y:{}, z:{}",x,y,z)
-// }
+fn main() {
+    let x: i32 = -56;
+    let y = 1000;
+    let z = 100.000;
+    println!("{}", x);
+    println!("{}", y);
 
-fn main(){
-    let is_male = true;
-    let is_above_18 = false;
-    if (is_male){
-        print!("You are a male.")
-    } else{
-        print!("You are not a male.")
+    print!("x:{}, y:{}, z:{}",x,y,z);
+
+    let mut a: i8 = 10;
+
+    // for i in 0..1000{
+    //     a = a + 100;
+    // }
+    println!("{}", a); // since compiler statically checks the code it never checks the code on runtime that's why 
+    // even after knowing that a is i8 it never corrects the code.
+
+    let is_male = false;
+    let mut is_above_18 = false;
+
+    // Since all the variables are mutable by default in rust we need to use mut if we want to change the value!!!
+
+    is_above_18 = true;
+    
+    if is_male {
+        println!("Yes that person is a male!!!");
+    } else {
+        println!("You are not a male!!!");
     }
+
     if is_male && is_above_18{
-        print!("You are a legal male.")
+        println!("You are a legal male!!!");
     }
-}
-// fn main(){
-//     let greeting = String::from("Hello World!!!");
-//     println!("{}", greeting);
 
-//     let char1 = greeting.chars().nth(1);
-//     print!("char1: {}", char1.unwrap())
-// }
+
+    // Strings
+
+    let greeting = String::from("Hello World!!");
+
+    println!("{}", greeting);
+
+    // println!("{}", greeting.chars().nth(1000));
+
+    // Conditionals
+
+    let is_even = 99;
+
+    if is_even % 2 == 0 {
+        println!("The number is even.")
+    } else if is_even % 2 == 1{
+        println!("The number is odd!!!")
+    }
+
+    // Conditionals
+
+    for i in 0..10{
+        println!("{}", i)
+    }
+    
+}
